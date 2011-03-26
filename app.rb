1837491611581dbf5002aa712197d7af826b5e9e
Max Gonzih <gonzih@gmail.com>
2011-03-26 12:54:11 +0200

    configured path for wkhtmltopdf

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'bundler'
 Bundler.require :default
 
 get '/pdf' do
+  PDFKit.configure do |config|
+    config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+    config.default_options = {
+    }
+  end
   content_type 'application/pdf'
   url = params[:url] =~ /^http:\/\// ? params[:url] : "http://#{params[:url]}"
   kit = PDFKit.new url
