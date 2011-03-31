86146faa9e7c6440fd7ff484c40eb86920b50f67
Max Gonzih <gonzih@gmail.com>
2011-03-31 21:08:12 +0300

    move setup to top, return string if url no specifyed

--- app.rb
+++ app.rb
@@ -2,20 +2,25 @@ require 'rubygems'
 require 'bundler'
 Bundler.require :default
 
+PDFKit.configure do |config|
+  config.wkhtmltopdf = 'wkhtmltopdf'
+  config.default_options = {
+    'margin-top' => 0,
+    'margin-bottom' => 0,
+    'margin-left' => 0,
+    'margin-right' => 0
+  }
+end
+
 get '/pdf' do
-  PDFKit.configure do |config|
-    config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
-    config.default_options = {
-      'margin-top' => 0,
-      'margin-bottom' => 0,
-      'margin-left' => 0,
-      'margin-right' => 0
-    }
+  if params[:url]
+    content_type 'application/pdf'
+    url = params[:url] =~ /^http:\/\// ? params[:url] : "http://#{params[:url]}"
+    kit = PDFKit.new url
+    kit.to_pdf
+  else
+    'specify url param'
   end
-  content_type 'application/pdf'
-  url = params[:url] =~ /^http:\/\// ? params[:url] : "http://#{params[:url]}"
-  kit = PDFKit.new url
-  kit.to_pdf
 end
 
 get '/' do
